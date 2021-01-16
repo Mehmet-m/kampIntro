7336a50d76972499d3d9239160ad3e5f9ceb340e
Mehmet Tiksöz <mehmettz456@gmail.com>
2021-01-17 01:06:00 +0200

    Ödevim =02.05 17Ock Pzr

---
+++ Odev/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Odev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.UrunAdi = "Notebook";
+            product1.UrunFiyati = 10000;
+
+            Product product2 = new Product();
+            product2.UrunAdi = "Televizyon";
+            product2.UrunFiyati = 5000;
+
+            Product product3 = new Product();
+            product3.UrunAdi = "Sweatshirt";
+            product3.UrunFiyati = 70;
+
+            Product[] products = new Product[] { product1, product2, product3 };
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.UrunAdi);
+                Console.WriteLine(product.UrunFiyati);
+            }
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Pub(product1);
+            sepetManager.Pub(product2);
+            sepetManager.Pub(product3);
+
+        }
+    }
+    class Product
+    {
+        public string UrunAdi { get; set; }
+        public int UrunFiyati { get; set; }
+    }
+        
+
+}
---
+++ Odev/SepetManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev
+{
+    class SepetManager
+    {
+        public void Pub(Product product)
+        {
+            Console.WriteLine("Ürün Sepete Eklendi :" + product.UrunAdi);
+            
+        }
+    }
+}
